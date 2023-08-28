a181d222c020adac60ef088c73d881965c3e4342
Nicolas Peredo <nicolas.peredo@globant.com>
2023-08-28 07:18:54 -0400

    Refactoring to use Portals

--- src/UI/Modal/Modal.js
+++ src/UI/Modal/Modal.js
@@ -1,27 +1,45 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import classes from "./Modal.module.scss";
 import Card from "../Card/Card";
 import Button from "../Button/Button";
 
+const Backdrop = ({ modalContent }) => {
+  return (
+    <div
+      className={classes["backdrop"]}
+      onClick={modalContent.dismissModalHandler}
+    ></div>
+  );
+};
+
+const ModalOverlay = ({ className, modalContent }) => {
+  return (
+    <Card className={`${classes["modal"]} ${className ? className : ""}`}>
+      <header className={classes["header"]}>
+        <h2>{modalContent.header}</h2>
+      </header>
+      <body className={classes["content"]}>{modalContent.body}</body>
+      <footer className={classes["actions"]}>
+        <Button onClickEventHandler={modalContent.dismissModalHandler}>
+          Close
+        </Button>
+      </footer>
+    </Card>
+  );
+};
+
 export default function Modal({ className, modalContent }) {
   return (
     <>
-      <div
-        className={classes["backdrop"]}
-        onClick={modalContent.dismissModalHandler}
-      >
-        <Card className={`${classes["modal"]} ${className ? className : ""}`}>
-          <header className={classes["header"]}>
-            <h2>{modalContent.header}</h2>
-          </header>
-          <body className={classes["content"]}>{modalContent.body}</body>
-          <footer className={classes["actions"]}>
-            <Button onClickEventHandler={modalContent.dismissModalHandler}>
-              Close
-            </Button>
-          </footer>
-        </Card>
-      </div>
+      {ReactDOM.createPortal(
+        <Backdrop modalContent={modalContent} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay className={className} modalContent={modalContent} />,
+        document.getElementById("overlay-root")
+      )}
     </>
   );
 }
